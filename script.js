2ae3c929c046087ef0172c3ae36c03e3bda8ee9e
Marshnme <jildsmiles@yahoo.com>
2022-06-23 16:05:11 -0400

    fetching lat and long of location

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-async function getWeatherData() {
+async function getWeatherData(cityInput, stateInput) {
+    getLatLon(cityInput, stateInput);
     const res = await fetch();
 }
+
+const getLatLon = async (cityInput, stateInput, countryCode) => {
+    try {
+        const res = await fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput},${stateInput},${countryCode}&limit=5&appid=b6845236a7811f015dd9b763581a389a`
+        );
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getLatLon("columbia", "SC", "USA");
