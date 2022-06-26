337989a8617ce79007dd6da30efe54a52f67c54f
Marshnme <jildsmiles@yahoo.com>
2022-06-25 20:14:35 -0400

    fixed some layout issues with grid

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const getLatLon = async (cityInput, stateInput, countryCode) => {
     try {
         const res = await fetch(
-            `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput},${stateInput},${countryCode}&limit=1&appid=b6845236a7811f015dd9b763581a389a`,
+            `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=b6845236a7811f015dd9b763581a389a`,
             { mode: "cors" }
         );
         const data = await res.json();
